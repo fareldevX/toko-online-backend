0d1928471a8500e24fd1cc6f98e1c395029a4239
Farel Arlish Orlando <mobillamborghini9999@gmail.com>
2025-11-08 11:23:01 +0700

    modified index.js

--- api/index.js
+++ api/index.js
@@ -2,10 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import multer from "multer";
-import path from "path";
 import Product from "../models/Product.js";
-import Photo from "../models/Photo.js";
 
 dotenv.config();
 
@@ -16,18 +13,9 @@ mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((e) => console.log(e));
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  },
-});
-const upload = multer({ storage });
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Server Running..." });
@@ -47,18 +35,6 @@ app.post("/api/product", async (req, res) => {
   } catch (e) {
     console.log(e);
   }
-});
-
-app.post("/upload", upload.single("image"), async (req, res) => {
-  const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`;
-
-  const photo = new Photo({
-    name: req.body.name,
-    imageUrl: imageUrl,
-  });
-  await photo.save();
-
-  res.status(201).json(photo);
 });
 
 export default app;
